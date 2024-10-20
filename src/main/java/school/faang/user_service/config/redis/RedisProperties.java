f0fedb133c9f065c3a935f3342178404b7161e20
Sergei Kochkin <lowerKamaCase@gmail.com>
2024-10-20 21:42:31 +0300

    BJS2-26167: Получение достижения Собиратора

--- src/main/java/school/faang/user_service/config/redis/RedisProperties.java
+++ src/main/java/school/faang/user_service/config/redis/RedisProperties.java
@@ -21,6 +21,7 @@ public class RedisProperties {
         private Channel followerEventChannel;
         private Channel mentorshipRequest;
         private Channel skillAcquiredChannel;
+        private Channel goalSetChannel;
 
         @Getter
         @Setter
---
+++ src/main/java/school/faang/user_service/event/goal/GoalSetEvent.java
@@ -0,0 +1,17 @@
+package school.faang.user_service.event.goal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter 
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoalSetEvent {
+    private Long userId;
+    private Long goalId;
+}
